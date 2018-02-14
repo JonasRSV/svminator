83120059a11ba3918821ece9f74c60a398efd212
Filip Lindvall <filip9719@gmail.com>
2018-02-14 17:26:34 +0100

    For the sake of my sanity, just ignore this...

--- src/svm.py
+++ src/svm.py
@@ -2,13 +2,13 @@ import random as rn
 import numpy as np
 from scipy.optimize import minimize
 import pandas as pd
-import matplotlib as mp
+import matplotlib.pyplot as plt
 
 PsuperVector = None
 LABELS = None
-SIZE = 20
-CLUSTER_SEP = 100
-
+SIZE = 40
+CLUSTER_SEP = 1
+GROUP_SPREAD = 1
 
 def sign(n):
     """K."""
@@ -23,26 +23,19 @@ def random_clusters(dim):
     global LABELS
     global CLUSTER_SEP
     global SIZE
+    global GROUP_SPREAD
 
-    LABELS = [sign(rn.randrange(-10, 10)) for _ in range(SIZE)]
-
+    LABELS = np.array([sign(rn.randrange(-10, 10)) for _ in range(SIZE)])
     separator = np.array([CLUSTER_SEP for _ in range(dim)])
 
     matrix_dim = (SIZE, dim)
     matrix = np.zeros(matrix_dim)
+
     for i in range(SIZE):
-        point = np.array([rn.random() for _ in range(dim)])
-
-        if LABELS[i] == 1:
-            point = point + separator
-        else:
-            point = point - separator
-
-        matrix[i] = point
+        point = np.array([GROUP_SPREAD * rn.random() for _ in range(dim)])
+        matrix[i] = point + LABELS[i] * separator
 
     dataFrame = pd.DataFrame(matrix)
-    dataFrame['label'] = LABELS
-
     return dataFrame
 
 
@@ -54,12 +47,14 @@ def simpleKern(a, b):
 def preComputePsuper(data, kern):
     """Simple kernel."""
     global PsuperVector
-    labelVector = np.matrix(data['label'])
-    PsuperVector = np.matmul(np.transpose(labelVector), (labelVector))
-
+    global LABELS
+    
+    PsuperVector = np.matmul(np.transpose(np.matrix(LABELS)), np.matrix(LABELS))
+    print(PsuperVector)
+    print(LABELS)
     for x, datax in data.iterrows():
         for y, datay in data.iterrows():
-            PsuperVector[x, y] *= kern(datax.drop(['label']), datay.drop(['label']))
+            PsuperVector[x, y] *= kern(datax, datay)
 
 
 def superSum(a):
@@ -87,26 +82,26 @@ def getTheFuckingAs(allAs):
 
 def calculateB(allAs, data, kern):
     """Fuck."""
-    s = data.iloc(0)
-    sum = 0
+    s = np.array(data.ix[1])
+    su = 0
     for idx, dat in data.iterrows():
         if idx in allAs:
-            sum += allAs[idx] * dat['label']\
-                * kern(s.drop('label'), dat.drop('label'))
+            su += allAs[idx] * LABELS[idx]\
+                * kern(s, dat)
 
-    return sum - s['label']
+    return su - LABELS[1]
 
 
 def indicatorFunc(allAs, b, data, kern, s):
     """Shit."""
-    sum = 0
+    su = 0
     for idx, dat in data.iterrows():
         if idx in allAs:
-            sum += allAs[idx] * dat['label'] * kern(dat.drop('label'), s)
-    return sum - b
+            su += allAs[idx] * LABELS[idx] * kern(dat, s)
+    return su - b
 
 
-dataFrame = random_clusters(4)
+dataFrame = random_clusters(2)
 preComputePsuper(dataFrame, simpleKern)
 print(superSum(np.zeros(SIZE)))
 
@@ -117,5 +112,32 @@ aaa = minimize(superSum, np.zeros(SIZE),
                bounds=np.array([(0, None) for _ in range(SIZE)]),
                constraints={'type': 'eq', 'fun': zerofun})
 
-print(aaa.x)
+print(aaa)
 
+aas = getTheFuckingAs(aaa.x)
+
+b = calculateB(aas, dataFrame, simpleKern)
+
+print(b)
+
+print(indicatorFunc(aas, b, dataFrame, simpleKern, np.array([100, 100])))
+print(indicatorFunc(aas, b, dataFrame, simpleKern, np.array([-100, -100])))
+
+dfw = dataFrame.copy()
+dfw['label'] = LABELS
+dp = dfw[dfw['label']==1]
+dp.columns = ['A', 'B', 'L']
+plt.scatter(dp['A'], dp['B'])
+
+dp = dfw[dfw['label']==-1]
+dp.columns = ['A', 'B', 'L']
+plt.scatter(dp['A'], dp['B'])
+
+xgrid = np.linspace(-2, 3)
+ygrid = np.linspace(-2, 3)
+
+grid = np.array([[indicatorFunc(aas, b, dataFrame, simpleKern, np.array([x,y])) for y in ygrid] for x in xgrid])
+
+plt.contour(xgrid, ygrid, grid, (-1.0, 0.0, 1.0), colors=('red', 'black', 'blue'), linewidth=(1,3,1))
+
+plt.show()
