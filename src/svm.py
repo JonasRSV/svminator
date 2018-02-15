5d95ceb2f0ba8a3ddcc775ab71497c087627c4cf
Filip Lindvall <filip9719@gmail.com>
2018-02-15 01:29:30 +0100

    syntax

--- src/svm.py
+++ src/svm.py
@@ -29,47 +29,62 @@ def random_clusters(group_spread, cluster_separation, number_of_points, dims):
 
         matrix[i] = point + labels[i] * separator
 
-    dataFrame = pd.DataFrame(matrix)
-    return labels, dataFrame
+    return np.matrix(labels), matrix
+
+def simpleKern(a, b):
+    """Linear Kernel Function."""
+    return np.dot(np.matrix(a).A1, np.matrix(b).A1)
 
 
 class Classifier(object):
     """SVM classifier object."""
 
-    def __init__(self, kernel_function=None):
+    def __init__(self, data, lables, kernel_function=simpleKern):
         """Constructor."""
         self.kernel_function = kernel_function
-        self.data = None
-        self.labels = None
+        self.data = data
+        self.labels = lables
+
         self.pre_processed_kernel = None
         self.bias = None
         self.support_vectors = None
 
-    def pre_process_classifiers(self, data, labels):
+    def pre_process_classifiers(self):
         """Preprocess first step of cost function."""
-        self.data = data
-        self.labels = labels
 
-        matrix_labels = np.matrix(self.labels)
+        self.pre_processed_kernel = np.matmul(labels.T, labels)
 
-        self.pre_processed_kernel =\
-            np.matmul(np.transpose(matrix_labels), matrix_labels)
-
-        for idx, datax in self.data.iterrows():
-            for idy, datay in self.data.iterrows():
+        for idx, rowx in enumerate(self.data):
+            for idy, rowy in enumerate(self.data):
                 self.pre_processed_kernel[idx, idy] *=\
-                    self.kernel_function(datax, datay)
+                    self.kernel_function(rowx, rowy)
+        print(self.pre_processed_kernel)
 
     def error_function(self, a):
         """Minimize for good SVM."""
-        lagrange_multiplies = np.matmul(a, np.transpose(a))
+        a = np.matrix(a)
+        lagrange_multiplies = np.matmul(a.T, a)
         kernel_values = lagrange_multiplies * self.pre_processed_kernel
 
-        return 0.5 * np.sum(kernel_values) - np.sum(a)
+        tmp = np.sum(kernel_values) - 2 * np.sum(a)
+        return tmp
 
-    def kernel_constrain(self, a):
+    def kernel_constrain(self, a, p=False):
         """Constraint for the lagrange thingy to work."""
-        return np.dot(a, self.labels) == 0
+        return np.dot(np.matrix(a).A1, self.labels.A1)
+
+    def determine_bias(self):
+        """Determine the bias of the SVM."""
+        sv_key = rn.choice(list(self.support_vectors.keys()))
+        a_sv = np.matrix(self.data[sv_key,:])
+
+        bias = 0
+        for index, point in enumerate(self.data):
+            if index in self.support_vectors:
+                bias += self.support_vectors[index]\
+                    * self.kernel_function(a_sv, point)
+
+        self.bias = bias - self.labels.A1[sv_key]
 
     def filter_lagrange_multipliers(self, list_of_multipliers):
         """Filter zero or practically zero values of a."""
@@ -78,32 +93,20 @@ class Classifier(object):
             if abs(sv) > 0.00001:
                 self.support_vectors[idx] = sv
 
-    def determine_bias(self):
-        """Determine the bias of the SVM."""
-        sv_key = rn.choice(self.support_vectors.keys())
-        a_sv = self.data[sv_key]
-
-        bias = 0
-        for index, point in self.data.iterrows():
-            if index in self.support_vectors:
-                bias += self.support_vectors[index]\
-                    * self.kernel_function(a_sv, point)
-
-        self.bias = bias - self.labels[sv_key]
-
-    def learn(self, data, labels, bounds):
+    def learn(self, bounds):
         """Create classifier."""
-        self.pre_process_classifiers(data, labels)
+        self.pre_process_classifiers()
 
-        point_cardinality = len(labels)
+        init_point = np.zeros(len(labels.A1))
 
-        lagrange_multipliers =\
+        minimize_ret =\
             minimize(self.error_function,
-                     point_cardinality,
+                     init_point,
                      bounds=bounds,
                      constraints={'type': 'eq',
-                                  'fun': self.kernel_constrain}).x
-
+                                  'fun': self.kernel_constrain})
+        print(minimize_ret)
+        lagrange_multipliers = minimize_ret.x
         self.filter_lagrange_multipliers(lagrange_multipliers)
         self.determine_bias()
 
@@ -111,55 +114,48 @@ class Classifier(object):
         """Classify a point."""
         classification = 0
         for key, lagrange_a in self.support_vectors.items():
-            classification += lagrange_a * self.labels[key]\
-                * self.kernel_function(point, self.data[key])\
+            classification += lagrange_a * self.labels.A1[key]\
+                * self.kernel_function(point, self.data[key,:])
 
         return classification - self.bias
 
+    def print(self):
 
-def simpleKern(a, b):
-    """Linear Kernel Function."""
-    return np.dot(a, b)
+        plt.scatter(self.data[:,0], self.data[:,1], c=['r' if i==1 else 'b' for i in self.labels.A1])
+
+        xgrid = np.linspace(-15, 15)
+        ygrid = np.linspace(-15, 15)
+
+        print("Bias", self.bias)
+
+    
+        grid = np.matrix([[self.indicator_func(np.matrix([x,y])) for y in ygrid] for x in xgrid])
+        
+        plt.contour(xgrid, ygrid, grid, (-1.0, 0.0, 1.0),
+                    colors=('red', 'black', 'blue'),
+                    linewidths=(1, 3, 1))
+        plt.show()
 
 
 GROUP_SPREAD = 10
 CLUSTER_SEPARATION = 10
-NUMBER_OF_POINTS = 20
-DIMENSION = 3
+NUMBER_OF_POINTS = 250
+DIMENSION = 2
 
 
-(labels, dataFrame) = random_clusters(GROUP_SPREAD,
-                                      CLUSTER_SEPARATION,
-                                      NUMBER_OF_POINTS,
-                                      DIMENSION)
+(labels, data) = random_clusters(GROUP_SPREAD,
+                                 CLUSTER_SEPARATION,
+                                 NUMBER_OF_POINTS,
+                                 DIMENSION)
+
+classifier = Classifier(data, labels)
 
 bounds = np.array([(0, None) for _ in range(NUMBER_OF_POINTS)])
-
-classifier = Classifier(kernel_function=simpleKern)
-
-print(dataFrame)
-
-classifier.learn(dataFrame, labels, bounds)
+classifier.learn(bounds)
+classifier.print()
 
 
 # print(indicatorFunc(aas, b, dataFrame, simpleKern, np.array([100, 100])))
 # print(indicatorFunc(aas, b, dataFrame, simpleKern, np.array([-100, -100])))
 
-# dfw = dataFrame.copy()
-# dfw['label'] = LABELS
-# dp = dfw[dfw['label']==1]
-# dp.columns = ['A', 'B', 'L']
-# plt.scatter(dp['A'], dp['B'])
 
-# dp = dfw[dfw['label']==-1]
-# dp.columns = ['A', 'B', 'L']
-# plt.scatter(dp['A'], dp['B'])
-
-# xgrid = np.linspace(-5, 5)
-# ygrid = np.linspace(-5, 5)
-
-# grid = np.array([[indicatorFunc(aas, b, dataFrame, simpleKern, np.array([x,y])) for y in ygrid] for x in xgrid])
-
-# plt.contour(xgrid, ygrid, grid, (-1.0, 0.0, 1.0), colors=('red', 'black', 'blue'), linewidth=(1,3,1))
-
-# plt.show()
